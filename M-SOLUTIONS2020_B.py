360e1155c0c73eccf29aa170d40c5627cac509ff
negibose2020 <allium.bose@gmail.com>
2020-07-26 14:35:57 +0900

    AC code for range K

--- M-SOLUTIONS2020_B.py
+++ M-SOLUTIONS2020_B.py
@@ -2,23 +2,20 @@ R,G,B=map(int,input().split())
 K=int(input())
 
 
-for i in range (K+1):
-    if R < G < B:
-        print ('Yes')
-        exit()
-    else:
-        pass
-    
-    if G < B:
-        pass
-    else:
-        B*=2
-        continue
-
-    if R < G:
+for _ in range (K) :
+    if R < G :
         pass
     else:
         G*=2
         continue
 
-print('No')
+    if G < B :
+        pass
+    else:
+        B*=2
+        continue
+    
+if R < G < B :
+    print('Yes')
+else :
+    print('No')
