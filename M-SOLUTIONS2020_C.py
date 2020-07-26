45af3efcb8d0d05884189ab5e758e9b752767160
negibose2020 <allium.bose@gmail.com>
2020-07-26 16:19:34 +0900

    AC but I saw explanation and I wrote this code

--- M-SOLUTIONS2020_C.py
+++ M-SOLUTIONS2020_C.py
@@ -1,22 +1,8 @@
 N,K=map(int,input().split())
 A=list(map(int,input().split()))
 
-dictS={}
-dictS[0]=1
-
-p=170141183460469231731687303715884105727
-
-for n in range (1,N):
-    if n<K:
-        # S.append(A[n]*S[-1])
-        dictS[n]=(A[n]*dictS[n-1])%p
-
-    else:
-        # S.append(A[n]*S[-1]/A[n-K])
-        dictS[n]=(A[n]*dictS[n-1]/A[n-K])%p
-
 for i in range (K,N):
-    if dictS[i]>dictS[i-1]:
+    if A[i]>A[i-K]:
         print('Yes')
     else:
         print('No')
