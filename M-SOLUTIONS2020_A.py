76425a1bf023b8b40f016add86bba7f64a4d37d7
negibose2020 <allium.bose@gmail.com>
2020-07-26 13:15:40 +0900

    AC but this code is not cool

---
+++ M-SOLUTIONS2020_A.py
@@ -0,0 +1,26 @@
+X=int(input())
+
+if 400<=X<=599:
+    print(8)
+    exit()
+if 600<=X<=799:
+    print(7)
+    exit()
+if 800<=X<=999:
+    print(6)
+    exit()
+if 1000<=X<=1199:
+    print(5)
+    exit()
+if 1200<=X<=1399:
+    print(4)
+    exit()
+if 1400<=X<=1599:
+    print(3)
+    exit()
+if 1600<=X<=1799:
+    print(2)
+    exit()
+if 1800<=X<=1999:
+    print(1)
+    exit()
