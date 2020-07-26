2b3d8fef727839c2c99a28ef9d7661a5ef76f82e
negibose2020 <allium.bose@gmail.com>
2020-07-26 15:53:02 +0900

    WA I thought maybe using MOD that is huge P number brings me AC

--- M-SOLUTIONS2020_C.py
+++ M-SOLUTIONS2020_C.py
@@ -4,14 +4,19 @@ A=list(map(int,input().split()))
 dictS={}
 dictS[0]=1
 
+p=170141183460469231731687303715884105727
+
 for n in range (1,N):
     if n<K:
-        dictS[n]=A[n]*dictS[n-1]
-    else:
-        dictS[n]=A[n]*dictS[n-1]/A[n-K]
+        # S.append(A[n]*S[-1])
+        dictS[n]=(A[n]*dictS[n-1])%p
 
-    if n>=K:
-        if dictS[n]>dictS[n-1]:
-            print('Yes')
-        else:
-            print('No')
+    else:
+        # S.append(A[n]*S[-1]/A[n-K])
+        dictS[n]=(A[n]*dictS[n-1]/A[n-K])%p
+
+for i in range (K,N):
+    if dictS[i]>dictS[i-1]:
+        print('Yes')
+    else:
+        print('No')
