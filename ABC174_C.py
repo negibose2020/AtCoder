f127a5d463e5c984f9f9b187de6b129de3c9302c
negibose2020 <allium.bose@gmail.com>
2020-08-04 00:38:04 +0900

    AC but I Look at the explanation,.

---
+++ ABC174_C.py
@@ -0,0 +1,23 @@
+# AtCoder Beginner Contest 174
+# C - Repsept
+
+K=int(input())
+
+modsevens=[7]
+
+if K==7 or K==1:
+    print(1)
+    exit()
+else:
+    pass
+
+for i in range (1,K+2):
+    a=(modsevens[-1] *10+7)%K
+    
+    if a==0:
+        print(i+1)
+        exit()
+    else:
+        modsevens.append(a)
+
+print(-1)
