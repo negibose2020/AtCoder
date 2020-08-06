39c18f45cbb43ccbfa42956c2512545ab617eb72
negibose2020 <allium.bose@gmail.com>
2020-08-06 17:43:47 +0900

    WA, after contest case...

---
+++ ABC086_C.py
@@ -0,0 +1,20 @@
+# AtCoder Beginner Contest 086
+# C - Traveling
+
+N=int(input())
+
+T=0
+X,Y=0,0
+
+for i in range (N):
+    t, x, y =map(int,input().split())
+    time =t-T
+    distance=(x+y)-(X+Y)
+    if time >= distance and time%2==distance%2:
+        T=t
+        X,Y=x,y
+    else :
+        print("No")
+        exit()
+
+print("Yes")
