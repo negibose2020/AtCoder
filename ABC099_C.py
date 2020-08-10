01cd12635c210be6e777eae8bd4f3eefe5c6e8be
negibose2020 <allium.bose@gmail.com>
2020-08-10 09:12:54 +0900

    AC I saw explanation

--- ABC099_C.py
+++ ABC099_C.py
@@ -1,5 +1,6 @@
 # AtCoder Regular Contest 099
 # C - Minimization 
+
 N,K =map(int,input().split())
 A=list(map(int,input().split()))
 
@@ -8,8 +9,27 @@ ans=0
 a=N-K
 ans+=1
 
-while a>0:
-    a+=1
-    ans+=1
-    a=a-K
+if a>0:
+    ans+=a//(K-1)
+    if a%(K-1)==0:
+        pass
+    else:
+        ans+=1
+else:
+    pass
+
 print(ans)
+
+# N,K =map(int,input().split())
+# A=list(map(int,input().split()))
+
+# ans=0
+
+# a=N-K
+# ans+=1
+
+# while a>0:
+#     a+=1
+#     ans+=1
+#     a=a-K
+# print(ans)
