54e8ca52cc6b96683c85ccb908d61a87ed26438a
negibose2020 <allium.bose@gmail.com>
2020-08-13 23:34:10 +0900

    TLE......

--- ABC127_C.py
+++ ABC127_C.py
@@ -8,14 +8,10 @@ N,M= map(int,input().split())
 # cards=[0]*N+1
 
 G=[0]*(N+1)
-Garr=np.array(G)
 
 for i in range (M):
     l,r=map(int,input().split())
-    g=[1 if ( l<=i and i<=r ) else 0 for i in range (N+1)]
-    garr=np.array(g)
-    Garr+=garr
+    for j in range(l,r+1):
+        G[j]+=1
 
-ans=np.count_nonzero(Garr==M)
-
-print (ans)
+print(G.count(M))
