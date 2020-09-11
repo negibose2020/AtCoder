98b104ac80723acff5556d7c6ea1f6aab7200cc4
negibose2020 <allium.bose@gmail.com>
2020-09-11 15:59:38 +0900

    AC

---
+++ ABC132_C.py
@@ -0,0 +1,19 @@
+# AtCoder Beginner Contest 132
+# C - Divide the Problems
+
+N=int(input())
+d=list(map(int,input().split()))
+
+d.sort()
+
+l=d[(N//2)-1]
+r=d[(N//2)]
+if l==r:
+    print(0)
+    exit()
+
+print(r-l)
+
+# print(d)
+# print(l,r)
+
