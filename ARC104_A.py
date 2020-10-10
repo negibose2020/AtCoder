01d429e949d24b1af3d98258b0bedd2d5a122acc
negibose2020 <allium.bose@gmail.com>
2020-10-10 16:03:05 +0900

    AC

---
+++ ARC104_A.py
@@ -0,0 +1,9 @@
+# AtCoder Regular Contest 104
+# A - Plus Minus
+
+A,B=map(int,input().split())
+for i in range (-100,101):
+    for j in range (-100,101):
+        if i+j==A and i-j==B:
+            print(i,j)
+            exit()
