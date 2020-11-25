9095f1b7188cb2feab5a5a94a87e012bd8fd13af
negibose2020 <allium.bose@gmail.com>
2020-11-25 06:09:46 +0000

    貰うDPで書き直し　AC

--- EDPC_C.py
+++ EDPC_C.py
@@ -10,16 +10,16 @@ dp=[[0]*3 for i in range (N)]
 dp[0][0]=abc[0][0]
 dp[0][1]=abc[0][1]
 dp[0][2]=abc[0][2]
-for i in range(N-1):
+for i in range(1,N):
     for j in range(3):
         if j==0:
-            dp[i+1][j]=max(dp[i+1][j],dp[i][1]+abc[i+1][0],dp[i][2]+abc[i+1][0])
+            dp[i][j]=max(dp[i-1][1]+abc[i][j],dp[i-1][2]+abc[i][j])
             # print(dp)
         if j==1:
-            dp[i+1][j]=max(dp[i+1][j],dp[i][2]+abc[i+1][1],dp[i][0]+abc[i+1][1])
+            dp[i][j]=max(dp[i-1][2]+abc[i][j],dp[i-1][0]+abc[i][j])
             # print(dp)
         if j==2:
-            dp[i+1][j]=max(dp[i+1][j],dp[i][0]+abc[i+1][2],dp[i][1]+abc[i+1][2])
+            dp[i][j]=max(dp[i-1][0]+abc[i][j],dp[i-1][1]+abc[i][j])
             # print(dp)
 
 print(max(dp[-1]))
