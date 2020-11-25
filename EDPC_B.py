85d23a8b51110f9ab0736f21021678df84b6a28a
negibose2020 <allium.bose@gmail.com>
2020-11-25 06:45:03 +0000

    AC

--- EDPC_B.py
+++ EDPC_B.py
@@ -1,18 +1,14 @@
 N,K=map(int,input().split())
-hi=list(map(int,input().split()))
+h=list(map(int,input().split()))
 
-dp=[10000000]*(N)
+dp=[10000]*(N)
 dp[0]=0
 for i in range (1,N):
-    dp[i]=min(dp[i],abs(hi[i]-hi[i-1])+dp[i-1])
-    # print(dp)
-    if 1 < i :
-        for j in range(1,K+1):
-            if j>i:
-                pass
-            else:
-                dp[i]=min(dp[i],abs(hi[i]-hi[i-j])+dp[i-j])
-            # print(dp)
+    for j in range (1,K+1):
+        if i-j<0:
+            continue
+        else:
+            dp[i]=min(dp[i],dp[i-j]+abs(h[i]-h[i-j]))
 
 # print(dp)
 print(dp[-1])
