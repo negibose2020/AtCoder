d6a41d4114c2a398b581dd84c73d6db2b4d56753
negibose2020 <allium.bose@gmail.com>
2020-11-30 08:26:32 +0000

    AC

--- PAST_4th/PAST4_F.py
+++ PAST_4th/PAST4_F.py
@@ -1,20 +1,39 @@
-def f(oS,tS):
-    if oS==tS:
-        return False
-    if tS==oS[::-1]:
-        return False
+import collections
+
+N,K=map(int,input().split())
+
+ls=[]
+for i in range (N):
+    s=input()
+    ls.append(s)
+
+Col=collections.Counter(ls)
+Com=Col.most_common()
+# for a in Col:
+    # print(a,Col[a])
+
+# print(Col)
+# print(Com[K-1])
+if K==1 and len(Com)==1:
+    print(Com[K-1][0])
+    exit()
+
+if K==1:
+    if Com[K-1][1]==Com[K][1]:
+        print("AMBIGUOUS")
     else:
-        return True
+        print(Com[K-1][0])
 
-import itertools
+elif K==len(Com):
+    if Com[K-1][1]==Com[K-2][1]:
+        print("AMBIGUOUS")
+    else:
+        print(Com[K-1][0])
 
-N=int(input())
-S=input()
-
-for v in itertools.permutations(S):
-    tS="".join(v)
-    if f(S,tS):
-        print(tS)
-        exit()
-    
-print("None")
+else:
+    if Com[K-1][1]==Com[K-2][1]:
+        print("AMBIGUOUS")
+    elif Com[K-1][1]==Com[K][1]:
+        print("AMBIGUOUS")
+    else:
+        print(Com[K-1][0])
