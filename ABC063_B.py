b77cdd8dd7bfd6b65811ad1b53b1cf9d1a03432a
negibose2020 <allium.bose@gmail.com>
2020-12-11 15:02:06 +0000

    AC

---
+++ ABC063_B.py
@@ -0,0 +1,12 @@
+# AtCoder Beginner Contest 063
+# B - Varied
+
+S=input()
+
+for i in range(len(S)-1):
+    for j in range (i+1,len(S)):
+        if S[i]==S[j]:
+            print("no")
+            exit()
+
+print("yes")
