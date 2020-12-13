58230896bcab809f9bf8d3498b75509aba621f0a
negibose2020 <allium.bose@gmail.com>
2020-12-13 08:14:41 +0000

    AC

---
+++ ABC090_B.py
@@ -0,0 +1,12 @@
+# AtCoder Beginner Contest 090
+# B - Palindromic Numbers
+
+A,B=map(int,input().split())
+
+ans=0
+
+for i in range (A,B+1):
+    s=str(i)
+    if s==s[::-1]:
+        ans+=1
+print(ans)
