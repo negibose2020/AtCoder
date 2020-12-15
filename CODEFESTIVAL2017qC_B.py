b9badc55d2a3f5b4f18053d90de25264986b9a87
negibose2020 <allium.bose@gmail.com>
2020-12-15 13:07:06 +0000

    解説見たけどわからず、結局全探索でAC

--- CODEFESTIVAL2017qC_B.py
+++ CODEFESTIVAL2017qC_B.py
@@ -1,2 +1,29 @@
-a=3
-print(a**10)
+# CODE FESTIVAL 2017 qual C
+# B - Similar Arrays
+
+from itertools import product
+
+N=int(input())
+A=list(map(int,input().split()))
+
+ls=[]
+ans=0
+
+for i in range (N):
+    templs=[]
+    for j in range(-1,2):
+        templs.append(A[i]+j)
+    ls.append(templs)
+
+d=list(product(range(3),repeat=N))
+
+for i in range(len(d)):
+    tempnum=1
+    for j in range (N):
+        tempnum*=ls[j][d[i][j]]
+    if tempnum%2==0:
+        ans+=1
+
+# print(ls)
+# print(d)
+print(ans)
