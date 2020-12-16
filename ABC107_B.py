4f3a8c4076c0210a93ba5396bc7eda28c5b3d97c
negibose2020 <allium.bose@gmail.com>
2020-12-16 14:01:07 +0000

    AC

--- ABC107_B.py
+++ ABC107_B.py
@@ -1,13 +1,33 @@
 import numpy as np
+import pprint
 
-# H,W=map(int,input().split())
-# e=[]
-# for i in range(H):
-#     _=list(input())
-#     e.append(_)
-# erea=np.array(e)
-# print(erea)
+H,W=map(int,input().split())
+e=[]
+for i in range(H):
+    a=list(input())
+    for i in range (W):
+        if a[i]=="#":
+            e.append(a)
+            break
+    else:
+        pass
 
-a = np.arange(12).reshape((3, 4))
-print(a)
+col=[]
 
+for i in range(len(e[0])):
+    for j in range (len(e)):
+        if e[j][i]=="#":
+            break
+        else:
+            pass
+    else:
+        col.append(i)
+
+col.reverse()
+# print(col)
+for i in range (len(col)):
+    for j in range (len(e)):
+        del e[j][col[i]]
+
+for i in range (len(e)):
+    print("".join(e[i]))
