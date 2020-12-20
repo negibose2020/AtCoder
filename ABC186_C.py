8e295215fd488fe172e63c74a3e8b6ad96a19f09
negibose2020 <allium.bose@gmail.com>
2020-12-20 14:52:36 +0000

    2nd commit

--- ABC186_C.py
+++ ABC186_C.py
@@ -1,10 +1,11 @@
-# パナソニックプログラミングコンテスト（AtCoder Beginner Contest 186）
+# パナソニックプログラミングコンテスト
+# AtCoder Beginner Contest 186
 # C - Unlucky 7
 
 N=int(input())
-ans=0
+ans=N
 for i in range (N+1):
     if ("7" in str(i)) or ("7" in str(oct(i))):
-        ans+=1
+        ans-=1
 
-print(N-ans)
+print(ans)
