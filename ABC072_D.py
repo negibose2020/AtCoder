2fc7b6dd1ba9690bdb9bc886e137bf9788eea426
negibose2020 <allium.bose@gmail.com>
2020-12-20 03:22:37 +0000

    AC

--- ABC072_D.py
+++ ABC072_D.py
@@ -3,20 +3,19 @@ p=list(map(int,input().split()))
 
 ans=0
 
-if p[0]==1 and p[1]==2:
-    pre=True
-else:
-    pre=False
+pre=False
 
 for i in range (N):
+    # print("start:{}:{}".format(i+1,pre))
     if p[i]==i+1:
         if pre==True:
             pre=False
-            pass
+            continue
         else:
             ans+=1
-            # print(i+1)
             pre=True
+            # print(i+1,pre)
+            continue
     pre=False
 print(ans)
 
