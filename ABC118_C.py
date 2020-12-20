4013a549104407f2adeca014f0397b2fe40b085f
negibose2020 <allium.bose@gmail.com>
2020-12-20 04:54:47 +0000

    ACしたけど余計な事していたようなので、解説見てgcdで解けた

--- ABC118_C.py
+++ ABC118_C.py
@@ -1,20 +1,29 @@
-def f(sortedlist):
-    if len(sortedlist)==1:
-        return sortedlist[0]
-    else:
-        newls=[sortedlist[0]]
-        for i in range(1,len(sortedlist)):
-            a=sortedlist[i]%sortedlist[0]
-            if a==0:
-                continue
-            else:
-                newls.append(a)
-        newls.sort()
-        return f(newls)
+# def f(sortedlist):
+#     if len(sortedlist)==1:
+#         return sortedlist[0]
+#     else:
+#         newls=[sortedlist[0]]
+#         for i in range(1,len(sortedlist)):
+#             a=sortedlist[i]%sortedlist[0]
+#             if a==0:
+#                 continue
+#             else:
+#                 newls.append(a)
+#         newls.sort()
+#         return f(newls)
+
+# N=int(input())
+# A=list(map(int,input().split()))
+
+# A.sort()
+
+# print(f(A))
+
+import math
 
 N=int(input())
 A=list(map(int,input().split()))
-
-A.sort()
-
-print(f(A))
+ans=A[0]
+for i in range (N):
+    ans=math.gcd(ans,A[i])
+print(ans)
