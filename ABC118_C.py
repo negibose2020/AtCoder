d957cb2f18df856877defbc2f819443459597768
negibose2020 <allium.bose@gmail.com>
2020-12-20 04:51:35 +0000

    AC

---
+++ ABC118_C.py
@@ -0,0 +1,20 @@
+def f(sortedlist):
+    if len(sortedlist)==1:
+        return sortedlist[0]
+    else:
+        newls=[sortedlist[0]]
+        for i in range(1,len(sortedlist)):
+            a=sortedlist[i]%sortedlist[0]
+            if a==0:
+                continue
+            else:
+                newls.append(a)
+        newls.sort()
+        return f(newls)
+
+N=int(input())
+A=list(map(int,input().split()))
+
+A.sort()
+
+print(f(A))
