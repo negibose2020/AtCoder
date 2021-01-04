561fbf2985235f8da7ba5e3cc2450070229ae1a5
negibose2020 <allium.bose@gmail.com>
2021-01-04 02:19:02 +0000

    自作クラスの取り扱いができずコンテスト中にAC出来ず。コンテスト翌日AC。

---
+++ ABC187_B.py
@@ -0,0 +1,35 @@
+# AtCoder Beginner Contest 187
+# B - Gentle Pairs
+
+class Coodinate (object):
+    def __init__(self,x=0,y=0,z=0):
+        self.x=x
+        self.y=y
+
+    def lineSlope(self,other):
+        deltax=self.x - other.x
+        deltay=self.y - other.y
+        slope=deltay/deltax
+        if -1<=slope<=1:
+            return True
+        else:
+            return False
+
+N=int(input())
+ls=[]
+
+ans=0
+
+for _ in range (N):
+    a,b =map(int,input().split())
+    p=Coodinate(a,b)
+    ls.append(p)
+
+for i in range (N):
+    for j in range (i+1,N):
+        if Coodinate.lineSlope(ls[i],ls[j]):
+            ans+=1
+        else:
+            pass
+
+print(ans)
