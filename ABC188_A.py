fc09fb6657e2a1930da569394cff001265a658f5
negibose2020 <allium.bose@gmail.com>
2021-01-12 14:05:56 +0000

    愚直に場合分け

--- ABC188_A.py
+++ ABC188_A.py
@@ -3,8 +3,13 @@
 
 X,Y=map(int,input().split())
 
-
-if abs(X-Y)<3:
-    print("Yes")
-else:
-    print("No")
+if X<Y:
+    if X+3>Y:
+        print("Yes")
+    else:
+        print("No")
+else:   # Y>X また、制約より Y==X はない。
+    if Y+3>X:
+        print("Yes")
+    else:
+        print("No")
