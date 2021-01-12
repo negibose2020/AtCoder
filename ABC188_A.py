bd365aa4c8b9acc8c6bfc40568f30604bc96993b
negibose2020 <allium.bose@gmail.com>
2021-01-12 13:58:17 +0000

    変数を大文字のXとYに変更

--- ABC188_A.py
+++ ABC188_A.py
@@ -1,8 +1,10 @@
 # AtCoder Beginner Contest 188
 # A - Three-Point Shot
 
-x,y=map(int,input().split())
-if abs(x-y)<3:
+X,Y=map(int,input().split())
+
+
+if abs(X-Y)<3:
     print("Yes")
 else:
     print("No")
