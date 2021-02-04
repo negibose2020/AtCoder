cf9bee2a513847d41ca9460717b1772834aaaf61
negibose2020 <allium.bose@gmail.com>
2021-02-04 22:28:47 +0900

    別解3

--- ABC190_A.py
+++ ABC190_A.py
@@ -3,7 +3,17 @@
 
 A,B,C=map(int,input().split())
 
-if A+C>B:
-    print("Takahashi")
-else:
-    print("Aoki")
+A+=C
+
+for i in range (100):
+    if i==0 and B==0 and A>0:
+        print("Takahashi")
+        exit()
+    A-=1
+    if A<=0:
+        print("Aoki")
+        exit()
+    B-=1
+    if B<=0:
+        print("Takahashi")
+        exit()
