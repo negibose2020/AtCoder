889c00261c6b5b29712bb71e89667ae764516b90
negibose2020 <allium.bose@gmail.com>
2021-03-02 04:23:35 +0000

    for文の上限を見直し

--- ABC193_C.py
+++ ABC193_C.py
@@ -1,36 +1,22 @@
 # キャディプログラミングコンテスト2021(AtCoder Beginner Contest 193)
 # C - Unexpressed
 
-def is_ok(num):
-    '''
-    okの条件を記載する。
-    個別の問題でそれぞれ定義する。
-    '''
-    a=i**num
-    if a<=n:
-        return True
-    else:
-        return False
+# print(10**10)     # 10000000000
+# print(2**34)      # 17179869184
+# print(3**34)      # 16677181699666569
+# print(4**34)      # 295147905179352825856
+# N=10**10
+N=int(input())
 
-def megru_bisect(ok,ng):
-    while(abs(ok-ng)>1):
-        mid=(ok+ng)//2
-        if is_ok(mid):
-            ok=mid
-        else:
-            ng=mid
-    return ok
+R=int(N**0.5)+1
+Nums=set()
 
-# n=10**10
-n=int(input())
-
-R=int(n**0.5)+1
-s=set()
-
-ans=n
 for i in range (2,R):
-    y=megru_bisect(1,100)
-    # ans-=y
-    for j in range (2,y+1):
-        s.add(i**j)
-print(n-len(s))
+    for j in range (2,34):
+        num=i**j
+        if num<=N:
+            Nums.add(num)
+        else:
+            break
+
+print(N-len(Nums))
