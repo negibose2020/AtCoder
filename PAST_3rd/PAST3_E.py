f247e0b063fc0195d378d37158eb2b46446a4476
negibose2020 <allium.bose@gmail.com>
2021-03-02 07:34:51 +0000

    隣接リスト使用でAC

--- PAST_3rd/PAST3_E.py
+++ PAST_3rd/PAST3_E.py
@@ -1,13 +1,16 @@
 N,M,Q=map(int,input().split())
-G=[[False for i in range (N)] for j in range (N)]
+# G=[[False for i in range (N)] for j in range (N)]
+G=[]
+for _ in range (N):
+    G.append([])
 # print(G)
 
 for _ in range (M):
     u,v=map(int,input().split())
     u-=1
     v-=1
-    G[u][v]=True
-    G[v][u]=True
+    G[u].append(v)
+    G[v].append(u)
 
 colors=list(map(int,input().split()))
 # print(G)
@@ -16,11 +19,9 @@ for _ in range (Q):
     if li[0]==1:
         x=li[1]-1
         print(colors[x])
-        for i in range (N):
-            if G[x][i]==False:
-                continue
-            else:
-                colors[i]=colors[x]
+        for i in range (len(G[x])):
+            t=G[x][i]
+            colors[t]=colors[x]
     else:
         x=li[1]-1
         print(colors[x])
