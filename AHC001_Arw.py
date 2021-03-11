0bf0c644efef75dbb42e0b874bff65a01785c303
negibose2020 <allium.bose@gmail.com>
2021-03-11 08:48:26 +0000

    隣接する四角でエリアを拡大したいものを拡大できるように変更

--- AHC001_Arw.py
+++ AHC001_Arw.py
@@ -44,10 +44,14 @@ def getData(boundaryLimits, INFO) :
     (区切られた)広告スペースと、入力から、以下の情報を返す。
     0 : 高さに関する情報。これは以下の3つの情報を持つ。
         0 : heightList
+            0 : height
+            1 : i
         1 : heightCnt
         2 : sameHeight_i
     1 : 幅に関する情報。これは以下の3つの情報を持つ。
         0 : widthList
+            0 : width
+            1 : i
         1 : widthCnt
         2 : sameWidth_i
     名前が良くない。。。
@@ -55,9 +59,9 @@ def getData(boundaryLimits, INFO) :
     start_x, start_y, end_x, end_y = map(int, boundaryLimits)
 
     height = [-1]*10000
-    heightList = [start_y - 1]
+    heightList = [[start_y - 1,-1]]
     width = [-1]*10000
-    widthList = [start_x - 1]
+    widthList = [[start_x - 1,-1]]
     heightCnt = 0
     widthCnt = 0
     sameHeight_i = set()
@@ -74,22 +78,27 @@ def getData(boundaryLimits, INFO) :
         else :
             sameHeight_i.add(i)
             sameHeight_i.add(height[y])
-        heightList.append(y)
+        # heightList.append(y)
+        heightList.append([y,i])
         
         if width[x] == -1 :
             width[x] = i
         else :
             sameWidth_i.add(i)
             sameWidth_i.add(width[x])
-        widthList.append(x)
+        # widthList.append(x)
+        widthList.append([x,i])
         
         heightCnt += 1
         widthCnt += 1
-    
-    heightList.sort()
-    widthList.sort()
-    
-    return [[heightList, heightCnt, sameHeight_i], [widthList, widthCnt, sameWidth_i]]
+
+    sortedHeightList=sorted(heightList, key = lambda x:x[0])
+    sortedWidthList=sorted(widthList, key= lambda y:y[0])
+    # heightList.sort()
+    # widthList.sort()
+
+    return [[sortedHeightList, heightCnt, sameHeight_i], [sortedWidthList, widthCnt, sameWidth_i]]
+    # return [[heightList, heightCnt, sameHeight_i], [widthList, widthCnt, sameWidth_i]]
 
 def calculateArea(v) :
     '''
@@ -104,6 +113,44 @@ def calculateArea(v) :
     return s
 
 def ReduceSize(INFO_i, v, n):
+    '''
+    サイズを小さくする基本的な機能を有している。
+    横縞と縦縞で、この関数をもとに、それぞれ別の関数として作成している。
+    # INFO_i : 入力で与えられる情報 [i,x,y,r]
+    # v : 四角形の頂点4つ [x1,y1,x2,y2]
+    # n : 企業数
+    x, y よりも左上にあるX1, y1 を x, y に近づけて得点を得られるvを返す。
+    v : [x1,y1,x2,y2]
+    '''
+    i, x, y, r = map(int, INFO_i)
+    x1, y1, x2, y2 = map(int, v)
+
+    w = x2 + 1 - x1
+    h = y2 + 1 - y1
+
+    area = calculateArea(v)
+
+    diff = area - r
+    fixVal = min(h, w)
+    lengthToCut = diff // fixVal
+    if fixVal == h and lengthToCut != 0 :
+        temp_x1 = x1 + lengthToCut
+        if x < temp_x1 :
+            x1 = x
+        else :
+            x1 = temp_x1
+    if fixVal == w and lengthToCut != 0 :
+        temp_y1 = y1 + lengthToCut
+        if y < temp_y1 :
+            y1 = y
+        else :
+            y1 = temp_y1
+    v = [x1, y1, x2, y2]
+
+    return v
+
+
+def ReduceSize_horizon(INFO_i, v, n):
     '''
     # INFO_i : 入力で与えられる情報 [i,x,y,r]
     # v : 四角形の頂点4つ [x1,y1,x2,y2]
@@ -120,20 +167,55 @@ def ReduceSize(INFO_i, v, n):
     area = calculateArea(v)
 
     diff = area - r
-    fixVal = min(w, h)
+    fixVal = w # 横縞と縦縞に関数を分離　fixValを幅に固定
     lengthToCut = diff // fixVal
-    if fixVal == h :
-        temp_x1 = x1 + lengthToCut
-        if x < temp_x1 :
-            x1 = x
-        else :
-            x1 = temp_x1
-    if fixVal == w :
+    # if fixVal == h and lengthToCut != 0 :
+    #     temp_x1 = x1 + lengthToCut
+    #     if x < temp_x1 :
+    #         x1 = x
+    #     else :
+    #         x1 = temp_x1
+    if fixVal == w and lengthToCut != 0 :
         temp_y1 = y1 + lengthToCut
         if y < temp_y1 :
             y1 = y
         else :
             y1 = temp_y1
+    v = [x1, y1, x2, y2]
+
+    return v
+
+def ReduceSize_vertic(INFO_i, v, n):
+    '''
+    # INFO_i : 入力で与えられる情報 [i,x,y,r]
+    # v : 四角形の頂点4つ [x1,y1,x2,y2]
+    # n : 企業数
+    x, y よりも左上にあるX1, y1 を x, y に近づけて得点を得られるvを返す。
+    v : [x1,y1,x2,y2]
+    '''
+    i, x, y, r = map(int, INFO_i)
+    x1, y1, x2, y2 = map(int, v)
+
+    w = x2 + 1 - x1
+    h = y2 + 1 - y1
+
+    area = calculateArea(v)
+
+    diff = area - r
+    fixVal = h # 横縞と縦縞に関数を分離　fixValを高さに固定
+    lengthToCut = diff // fixVal
+    if fixVal == h and lengthToCut != 0 :
+        temp_x1 = x1 + lengthToCut
+        if x < temp_x1 :
+            x1 = x
+        else :
+            x1 = temp_x1
+    # if fixVal == w and lengthToCut != 0 :
+    #     temp_y1 = y1 + lengthToCut
+    #     if y < temp_y1 :
+    #         y1 = y
+    #     else :
+    #         y1 = temp_y1
     v = [x1, y1, x2, y2]
 
     return v
@@ -219,6 +301,75 @@ def CutExcessPart_vertic(INFO_i, v, boundaryLimits, n) :
 
     return v
 
+def compensation_horizon(compensationData_horizon,INFO,horizontalStripesDic,horizontalStripesScore):
+    score=horizontalStripesScore
+    dic=horizontalStripesDic
+    for check_i in range (len(compensationData_horizon)):
+        checkIndex=compensationData_horizon[check_i][0]
+        if checkIndex==-1:
+            continue
+        possibleRange=compensationData_horizon[check_i][1]
+        r=INFO[checkIndex][3]
+        v=dic[checkIndex]
+        area=calculateArea(v)
+
+        if area > r :
+            continue
+        else:
+            score -= calculateScore(v,r,len(INFO))
+            x1,y1,x2,y2=map(int,v)
+            diff= r-area
+            w = x2-x1
+            moveDist=diff//w
+            temp_y2=y2+moveDist
+            if temp_y2 > possibleRange:
+                y2=possibleRange
+                # v = [x1,y1,x2,y2]
+            else:
+                y2=temp_y2
+            v = [x1,y1,x2,y2]
+            dic[checkIndex] = v
+            score += calculateScore(v,r,len(INFO))
+
+    return [score,dic]
+
+
+
+def compensation_vertic (compensationData_vertical,INFO,verticalStripesDic,verticalStripesScore):
+    score=verticalStripesScore
+    dic=verticalStripesDic
+    for check_i in range (len(compensationData_vertical)):
+        checkIndex=compensationData_vertical[check_i][0]
+        if checkIndex==-1:
+            continue
+        possibleRange=compensationData_vertical[check_i][1]
+        r=INFO[checkIndex][3]
+        v=dic[checkIndex]
+        area=calculateArea(v)
+
+        # print(0,dic[checkIndex],possibleRange)
+        
+        if area > r :
+            continue
+        else:
+            score -= calculateScore(v,r,len(INFO))
+            x1,y1,x2,y2=map(int,v)
+            diff= r- area
+            h = y2 -y1
+            moveDist=diff//h
+            temp_x2=x2+moveDist
+            if temp_x2 > possibleRange:
+                x2=possibleRange
+            else:
+                x2=temp_x2
+            v = [x1,y1,x2,y2]
+            dic[checkIndex] = v
+            score += calculateScore(v,r,len(INFO))
+
+        # print(1,dic[checkIndex])
+
+    return [score,dic]
+
 
 
 def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
@@ -232,12 +383,26 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
     verticalStripesDic = dict()
 
     data = getData(boundaryLimits, INFO)
-    heightList = data[0][0]
+    heightListData = data[0][0]
+    heightList=[]
+    heightDic=dict()
+    for heightList_i in range (len(heightListData)):
+        heightList.append(heightListData[heightList_i][0])
+        heightDic[heightListData[heightList_i][0]]=heightListData[heightList_i][1]
+
     heightCnt = data[0][1]
     sameHeight_i = data[0][2]
-    widthList = data[1][0]
+    widthListData = data[1][0]
+    widthList=[]
+    widthDic=dict()
+    for widthList_i in range (len(widthListData)):
+        widthList.append(widthListData[widthList_i][0])
+        widthDic[widthListData[widthList_i][0]]=widthListData[widthList_i][1]
     widthCnt = data[1][1]
     sameWidth_i = data[1][2]
+
+    compensationData_horizon=[]
+    compensationData_vertical=[]
 
     # ここから横縞の計算 主に高さを使用して計算を進める
     for INFO_i in range (n) :
@@ -251,6 +416,7 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
             horizontalStripesDic[i] = v
             continue
 
+        
         heightOder = heightList.index(y)
         preHeightOder = heightOder -1
         baseOfBorderingOnTop = heightList[preHeightOder]
@@ -265,11 +431,21 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
             y2 = end_y
             v = [x1, y1, x2, y2]
         
+        # 上側を削る処理
+        if area > r :
+            y1_before=v[1]
+            v = ReduceSize_horizon(INFO[INFO_i], v, n)
+            area = calculateArea(v)
+            y1_after=v[1]
+            if y1_after != y1_before:
+                preIndex_INFO_i=heightDic[baseOfBorderingOnTop]
+                compensationData_horizon.append([preIndex_INFO_i,y1_after])
+
         # 左側を削る処理
         if area > r :
             v = ReduceSize(INFO[INFO_i], v, n)
             area = calculateArea(v)
-        
+
         # 右側を削る処理
         if area > r :
             v = CutExcessPart_horizon(INFO[INFO_i],v,boundaryLimits,n)
@@ -277,6 +453,7 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
         
         horizontalStripesScore += calculateScore(v,r,n)
         horizontalStripesDic[i]= v
+    
 
     # ここから縦縞の計算
     for INFO_i in range (n) :
@@ -304,9 +481,19 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
             x2 = end_x
             v = [x1, y1, x2, y2]
 
+        # 左側を削る処理
+        if area > r :
+            x1_before=v[0]
+            v = ReduceSize_vertic(INFO[INFO_i], v, n)
+            area = calculateArea(v)
+            x1_after=v[0]
+            if x1_after != x1_before:
+                preIndex_INFO_i=widthDic[baseOfBorderingOnLeft]
+                compensationData_vertical.append([preIndex_INFO_i,x1_after])
+
         # 上側を削る処理
         if area > r :
-            v = ReduceSize(INFO[INFO_i], v, n)
+            v = ReduceSize(INFO[INFO_i],v,n)
             area = calculateArea(v)
 
         # 下側を削る処理
@@ -316,7 +503,14 @@ def decideSizeAdsUnderBoundaryLimits(boundaryLimits, INFO) :
 
         verticalStripesScore += calculateScore(v,r,n)
         verticalStripesDic[i]= v
-    
+
+    horizontalStripes_after=compensation_horizon(compensationData_horizon,INFO,horizontalStripesDic,horizontalStripesScore)
+    horizontalStripesScore=horizontalStripes_after[0]
+    horizontalStripesDic=horizontalStripes_after[1]
+    verticalStripes_after=compensation_vertic(compensationData_vertical,INFO,verticalStripesDic, verticalStripesScore)
+    verticalStripesScore=verticalStripes_after[0]
+    verticalStripesDic=verticalStripes_after[1]
+
     if horizontalStripesScore >= verticalStripesScore:
         score=horizontalStripesScore
         dic=horizontalStripesDic
@@ -390,9 +584,15 @@ for iP_i in range (len(intersectionPoints)):
         dic4ans=dict()
         dic4ans=d
 
+# for ans_i in range(N):
+#     # print(*dic4ans[ans_i])
+#     print(ans_i,dic4ans[ans_i])
+#     print(INFO[ans_i])
+#     print(r-calculateArea(dic4ans[ans_i]),r>calculateArea(dic4ans[ans_i]))
+#     print("-------------------")
+
 for ans_i in range(N):
     print(*dic4ans[ans_i])
-
 
 
 '''
